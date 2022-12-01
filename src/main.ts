ea0845c248fd4566348200a7889a579a054440e1
Lucien77144 <79203857+Lucien77144@users.noreply.github.com>
2022-12-01 16:30:14 +0100

    feat(game): tout

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import * as Matter from 'matter-js'
 
+import 'pathseg'
+
+import losangeSVG from './svg/losange.svg';
+import vert from './svg/vert.svg';
+import rouge from './svg/rouge.svg';
+
 const {
+  Common,
+  Vertices,
+  Vector,
+  Svg,
   Events,
   Body,
   Engine,
@@ -14,11 +24,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const app = document.getElementById('app')
   if (!app) throw new Error("app wasn't found in dom")
 
-  window.addEventListener('resize', () => {
-    render.canvas.width = window.innerWidth;
-    render.canvas.height = window.innerHeight;
-  });
-
   const sizes = {
       x: window.innerWidth,
       y: window.innerHeight,
@@ -29,6 +34,12 @@ document.addEventListener('DOMContentLoaded', () => {
           little: 20,
       },
   };
+  let players: {}[] = [];
+
+  window.addEventListener('resize', () => {
+    render.canvas.width = sizes.x;
+    render.canvas.height = sizes.y;
+  });
 
   // create an engine
   const engine = Engine.create()
@@ -47,26 +58,95 @@ document.addEventListener('DOMContentLoaded', () => {
   render.canvas.height = sizes.y;
 
   // create two boxes and a ground
-  const boxA = Bodies.rectangle(400, 200, 80, 80);
-  const boxB = Bodies.rectangle(450, 50, 80, 80);
-  // const polygon1 = Bodies.polygon(100, 100, 4, 50);
-  const polygon1 = Bodies.trapezoid(100, 100, 50, 50, 0.5);
-  const player = Bodies.circle(100, 100, sizes.players.base);
 
-  const borderWidth = 2000;
-  const wallTop = Bodies.rectangle(sizes.x/2+(borderWidth/2), -(borderWidth/2), sizes.x+borderWidth, borderWidth, { isStatic:true, render: { fillStyle: 'transparent' } });
-  const wallBottom = Bodies.rectangle(sizes.x/2+(borderWidth/2), sizes.y+(borderWidth/2), sizes.x+borderWidth, borderWidth, { isStatic:true, render: { fillStyle: 'transparent' } });
-  const wallLeft = Bodies.rectangle(-(borderWidth/2), sizes.y/2+(borderWidth/2), borderWidth, sizes.y+borderWidth, { isStatic:true, render: { fillStyle: 'transparent' } });
-  const wallRight = Bodies.rectangle(sizes.x+(borderWidth/2), sizes.y/2+(borderWidth/2), borderWidth, sizes.y+borderWidth, { isStatic:true, render: { fillStyle: 'transparent' } });
+  const bw = 2000;
+  const wallTop = Bodies.rectangle(sizes.x/2+(bw/2), -(bw/2), sizes.x+bw, bw, { isStatic:true, render: { fillStyle: 'transparent' } });
+  const wallBottom = Bodies.rectangle(sizes.x/2+(bw/2), sizes.y+(bw/2), sizes.x+bw, bw, { isStatic:true, render: { fillStyle: 'transparent' } });
+  const wallLeft = Bodies.rectangle(-(bw/2), sizes.y/2+(bw/2), bw, sizes.y+bw, { isStatic:true, render: { fillStyle: 'transparent' } });
+  const wallRight = Bodies.rectangle(sizes.x+(bw/2), sizes.y/2+(bw/2), bw, sizes.y+bw, { isStatic:true, render: { fillStyle: 'transparent' } });
+
+  let indexLosanges = 0;
+  const createLosange = (x=500, y=500, type=vert, scale = {x: .4, y: .4}) => {
+    const select = (root: any, selector:any) => {
+      return Array.prototype.slice.call(root.querySelectorAll(selector));
+    };
+    const loadSvg = (url: string) => {
+      return fetch(url)
+        .then(function(response) { return response.text(); })
+        .then(function(raw) { return (new window.DOMParser()).parseFromString(raw, 'image/svg+xml'); });
+    };
+  
+    let result;
+    return loadSvg(losangeSVG).then((root) => {
+      const vertexSets = select(root, 'path').map(function(path) {
+        return Vertices.scale(
+          Svg.pathToVertices(path, 30),
+          scale.x,
+          scale.y,
+          Vector.create(100, 100)
+        );
+      });
+      result = Bodies.fromVertices(
+        x, // x 
+        y, // y
+        vertexSets,   
+        {
+          frictionAir: .1,
+          restitution: .25,
+          render: {
+            sprite: {
+              texture: vert,
+              xScale: scale.x,
+              yScale: scale.y,
+            },
+          }
+        },
+        true,
+      )
+      Composite.add(
+        engine.world,
+        result
+      );
+      result.game = {
+        id: indexLosanges,
+        type: 'life',
+        scale: 1,
+      };
+      indexLosanges++;
+      return result;
+    });
+  }
+  for (let i = 0; i < 10; i++) {
+    createLosange(Math.random()*sizes.x, Math.random()*sizes.y, vert, {x: .4, y: .4});
+  }
 
   engine.gravity.y = 0;
-  console.log(engine)
   
-  Composite.add(engine.world, [boxA, boxB, player, polygon1, wallBottom, wallTop, wallLeft, wallRight]);
+  Composite.add(engine.world, [wallBottom, wallTop, wallLeft, wallRight]);
 
+  window.addEventListener('gamepadconnected', function(e) {
 
-  window.addEventListener('gamepadconnected', function() {
-    console.log('gamepad connected');
+    for (let i = 0; i < 2; i++) {
+      const newPlayer = Bodies.circle(
+        sizes.x/2,
+        sizes.y/2,
+        sizes.players.base,
+        {
+          restitution: .75,
+        }
+      );
+
+      players.push(newPlayer);
+      newPlayer.game = {
+        id: i,
+        type: 'player',
+        health: 10,
+        secure: false,
+      }
+      
+      Composite.add(engine.world, [newPlayer]);
+    }
+    console.log('gamepads connected');
   });
 
   // run the renderer
@@ -78,9 +158,52 @@ document.addEventListener('DOMContentLoaded', () => {
   Events.on(runner, 'afterTick', (event) => {
     const speedfactor = 15;
     const pad = navigator.getGamepads() && navigator.getGamepads()[0];
-    const [padX, padY] = pad?.axes || [0,0];
+    const [padXJ1, padYJ1, padXJ2, padYJ2] = pad?.axes || [0,0];
     
-    Body.setVelocity(player, { x: padX*speedfactor, y: padY*speedfactor });
+    if(players.length > 0) {
+      Body.setVelocity(players[0], { x: padYJ1*speedfactor, y: -padXJ1*speedfactor });
+      Body.setVelocity(players[1], { x: -padYJ2*speedfactor, y: padXJ2*speedfactor });
+    }
+  });
+
+  Events.on(engine, 'collisionStart', (event) => {
+    const e1 = event.pairs[0].bodyA;
+    const e2 = event.pairs[0].bodyB;
+
+    if(!e1.game || !e2.game) return;
+
+    if(e2.game.type === 'player' && e1.game.type === 'life') {
+      console.log(`player${e2.game.id} get ${e1.game.scale} life points`);
+      e2.game.health += e1.game.scale;
+      console.log(e2.game);
+      
+    } else if (e1.game.type === 'player' && e2.game.type === 'life') {
+      console.log(`player${e1.game.id} get ${e2.game.scale} life points`);
+      e1.game.health += e2.game.scale;
+    }
+
+    if(e2.game.type === 'player' && e1.game.type === 'player') {
+      if(e2.game.health > e1.game.health) {
+        if(!e1.game.secure) {
+          e1.game.health = e1.game.health-1;
+          e1.game.secure = true;
+          setTimeout(() => {
+            e1.game.secure = false;
+          }, 1000);
+        }
+      } else if (e2.game.health < e1.game.health) {
+        if(!e2.game.secure) {
+          e2.game.health = e2.game.health-1;
+          e2.game.secure = true;
+          setTimeout(() => {
+            e2.game.secure = false;
+          }, 1000);
+        }
+      }
+    }
+    e2.circleRadius = e2.game.health * 4;
+    e1.circleRadius = e1.game.health * 4;
+
   });
 
   // run the engine
